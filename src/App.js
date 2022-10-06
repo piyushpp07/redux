62e8bb8dc114d36d5254d92c9f2cec681df43ccd
piyushpp07 <piyushpar7@gmail.com>
2022-10-06 10:02:39 +0530

    Done basic redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux/es/exports';
-
+import { actions } from './store/index'
 //dispatch is use to send the data to redux store
 //selector used to find the data from the redux store 
 
 function App() {
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch({ type: 'INC' })
+    dispatch(actions.increment())
   }
   const decrement = () => {
-    dispatch({ type: 'DEC' })
+    dispatch(actions.decrement())
   }
   const addBy = () => {
-    dispatch({ type: 'ADD', payload: 10 })
+    dispatch(actions.addBy(10))
   }
   const counter = useSelector((state) => state.counter);
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,25 @@
-import { createStore } from 'redux';
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const reducerFxn = (state = { counter: 0 }, action) => {
-
-   //synchronous function
-   //we should not mutate the orignal state it will crash the app
-
-   if (action.type === 'INC') {
-      return { counter: state.counter + 1 };
+const counterSlice = createSlice({
+   name: 'counter',
+   initialState: { counter: 0 },
+   reducers: {
+      increment(state, action) {
+         state.counter++
+      },
+      decrement(state, action) {
+         state.counter--
+      },
+      addBy(state, action) {
+         state.counter += action.payload
+      }
    }
-   if (action.type === 'DEC') {
-      return { counter: state.counter - 1 };
-   }
-   if (action.type === 'ADD') {
-      return { counter: state.counter + action.payload }
-   }
-   return state;
-}
 
-const store = createStore(reducerFxn);
+})
+
+export const actions = counterSlice.actions
+
+const store = configureStore({
+   reducer: counterSlice.reducer
+})
 export default store;
